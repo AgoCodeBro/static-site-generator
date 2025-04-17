82b47addec78bc31f81913ddb83175d74e453547
AgoCodeBro <agoimbimboparra@gmail.com>
2025-04-17 22:24:57 +0000

    added a static directory, and content in public is now generated using  copy directory function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,42 @@
 from textnode import TextNode, TextType
+import os
+import os.path as osp
+import shutil
 
 def main():
-    node = TextNode("testing", TextType.BOLD, "www.testlol.com")
-    print(node)
+    copy_directory("static", "public")
+
+def copy_directory(source, destination):
+    if not osp.exists(source):
+        raise ValueError("Source path not found")
+
+    if osp.exists(destination):
+        try:
+            shutil.rmtree(destination)
+
+        except:
+            raise ValueError("Destination given should be a directory")
+        
+    
+    os.mkdir(destination)
+    source_content = os.listdir(source)
+    for item in source_content:
+        item_path = f"{source}/{item}"
+
+        if osp.isfile(item_path):
+            shutil.copy(item_path, destination)
+
+        elif osp.isdir(item_path):
+            copy_path = f"{destination}/{item}"
+            os.mkdir(copy_path)
+            copy_directory(item_path, copy_path)
+
+
+
+            
+    
+
+
 
 if __name__ == "__main__":
     main()
