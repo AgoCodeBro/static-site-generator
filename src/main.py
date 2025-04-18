f708e12985012e0ab53ad0c643d00c993a56bd90
AgoCodeBro <agoimbimboparra@gmail.com>
2025-04-18 18:32:05 +0000

    fixed pathing and regenrated the pages

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ import sys
 
 def main():
     basepath = "/"
-    if len(sys.argv) > 0:
-        basepath = sys.argv[0]
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     copy_directory("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
